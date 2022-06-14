6a376326d0d86b8468bf37adff0f095726f1f5bc
Daniil Fedotov <fedot13887@gmail.com>
2022-06-14 19:54:48 +0300

    add task controle works

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿string[] enArray = Console.ReadLine()!.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+string[] putArray = new string[enArray.Length];
+
+
+putArray = ColleArray(enArray, putArray);
+Console.WriteLine(String.Join(",", putArray));
+
+
+string[] ColleArray(string[] enArray, string[] putArray)
+{
+    int i = 0;
+    while (i < enArray.Length)
+    {
+        if (enArray[i].Length <= 3)
+        {
+            putArray[i] = enArray[i];
+
+        }
+        i++;
+    }
+    return putArray;
+}
